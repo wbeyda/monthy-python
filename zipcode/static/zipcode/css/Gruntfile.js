a8fd37eab563642bd3890ef913be60f836d1c242
wbeyda <warrenbeyda@gmail.com>
2015-11-10 03:45:52 -0700

    "added grunt packages uglify and concat and made configs to concatinate all CSS and JS files down to one file."

--- zipcode/static/zipcode/css/Gruntfile.js
+++ zipcode/static/zipcode/css/Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        
         less: {
           development: {
           options: {
@@ -14,13 +13,66 @@ module.exports = function(grunt) {
             }
           }
         },
+
+        concat: {
+            options: {
+                separator: "",
+            },
+            style: {
+                src: ["style.css", "calendar.css", "gallery.css", 'main.css', 'site.css', 'bootstrap.min.css', 'font-awesome.min.css', 'jquery.mobile-1.4.5.css', '../js/jquery-ui-1.9.2.custom/css/ui-lightness/jquery-ui-1.9.2.custom.css', 'normalize.min.css'],
+                dest: 'site.min.css',
+            },
+        }, 
+
+        uglify: {
+            dev: {
+                options : {
+                    mangle: false,
+                    compress: false,
+                    beautify: true
+                },
+                src: ["../js/jquery-2.1.1.js",
+                      "../js/jquery-ui-1.9.2.custom/js/jquery-ui-1.9.2.custom.js",
+                      "../js/bootstrap.js",
+                      "../js/animatescroll.js", 
+                      "../js/main.js",
+                      "../js/plugins.js",
+                      "../js/vendor/modernizr-2.8.3.min.js",
+                     ],
+                dest:'../js/ugly.min.js',
+
+            },
+            dist: {
+                options: {
+                    mangle: true,
+                    compress: true
+                },
+                src: ['../js/ugly.min.js'],
+                dest: '../js/main.min.js'
+            },
+        },
+
         watch: {
-            files: "less/**/*.less",
-            tasks: ["less"]
-        }
+            less: {
+                files: ["less/**/*.less"],
+                tasks: ["less"],
+            },
+            concat: {
+                files: ["*.css"],
+                tasks: ["concat:style"],
+            },
+            uglify: {
+                files: ["../js/*.js"],
+                tasks: ["uglify:dev"],
+
+            },
+        },
+        
     })
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.registerTask( 'default', ['less','watch'] );
 };
 
--- zipcode/static/zipcode/js/calendar.js
+++ zipcode/static/zipcode/js/calendar.js
@@ -1,3 +1,4 @@
+var = nothing = "dicks";
 if (window.jQuery) {
     $(document).ready(function() {
 
